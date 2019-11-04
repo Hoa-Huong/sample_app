388d6de4999ccd63c4ed516607bdbab3f1279842
Hoa-Huong <hoahuongad@gmail.com>
2019-11-04 08:22:42 +0700

    Chapter 13: user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_101727) do
+ActiveRecord::Schema.define(version: 2019_11_03_033359) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_10_30_101727) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
